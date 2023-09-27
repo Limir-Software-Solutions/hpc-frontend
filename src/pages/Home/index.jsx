444ae2b48228668378958b1e33d6d5e106cdd063
Lukas <lukas_flores.8@hotmail.com>
2023-09-27 20:22:47 -0300

    changes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Avatar, Button} from "@mui/material";
+
 
 const Home = () => {
   return (
-    <div>
-      HOLA MUNDO
-    </div>
+      <>
+        <Button
+            variant="contained"
+            sx={{ backgroundColor: 'red', color: 'white'}}
+        >HOLA</Button>
+      </>
   );
 };
 
