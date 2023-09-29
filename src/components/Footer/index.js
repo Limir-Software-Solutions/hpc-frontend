41b52cdbad36675f5f26cd7fab9716686aed0337
rodri <rodrigo.llancao@pregrado.uoh.cl>
2023-09-29 00:48:12 -0300

    add footer2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,10 +6,55 @@ import IconButton from '@mui/material/IconButton';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 const Footer = () => {
     
     return (
+        <>
+        <AppBar position="static" sx={{ backgroundColor: 'black' }}>
+            <Toolbar>
+                <Typography variant="body1" color="inherit" sx={{ flexGrow: 1 }}>
+                    HPC UOH High Performance Computer
+                </Typography>
+                <List dense sx={{ display: 'flex', flexDirection: 'column',marginRight:"1400px" }}>
+                        <ListItem>
+                            <ListItemText primary="Nosotros"
+                                        primaryTypographyProps={{ color: '#61dafb' }} />
+                            
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Acerca de" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Equipo" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Publicaciones" />
+                        </ListItem>
+                    </List>
+                    <List dense sx={{ display: 'flex', flexDirection: 'column',marginRight:"1300px" }}>
+                        <ListItem>
+                            <ListItemText primary="Más"
+                                        primaryTypographyProps={{ color: '#61dafb' }} />
+                            
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Infraestructura" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Noticias" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Visitas" />
+                        </ListItem>
+                    </List>    
+                
+            </Toolbar>
+        </AppBar>
+        
         <AppBar position="static" color="primary">
             <Toolbar>
                 <Typography variant="body1" color="inherit" sx={{ flexGrow: 1 }}>
@@ -26,6 +71,7 @@ const Footer = () => {
                 </IconButton>
             </Toolbar>
         </AppBar>
+        </>
     );
 }
 
