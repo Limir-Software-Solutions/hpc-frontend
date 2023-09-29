d09855236dbdd01577e3999ff76bebb380ce0aac
IvanBozoCa <133916421+IvanBozoCa@users.noreply.github.com>
2023-09-28 23:01:05 -0300

    Imagen vistas cards

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import ImagenCard from './Imagencards';
 
 
 const Home = () => {
   return (
       <>
           <h1>Laboratorio de Computación de Alto Rendimiento UOH</h1>
+          <ImagenCard />
+          <ImagenCard />
+          <ImagenCard /> 
       </>
 
   );
