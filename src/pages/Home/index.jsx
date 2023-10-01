5b7961a9972c029888f8debeafb3a60812bac6ff
Lukas <lukas_flores.8@hotmail.com>
2023-10-01 17:23:10 -0300

    carousel component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,38 +1,46 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import {CardActionArea, Container} from '@mui/material';
-import Stack from '@mui/material/Stack';
+import { Container} from '@mui/material';
 import Cards from '../../components/Cards';
+import Carousel from '../../components/Carousel';
+
 export default function Home() {
+
+  const slides = [
+    "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
+    "https://wallpapercave.com/wp/wp3386769.jpg",
+    "https://wallpaperaccess.com/full/809523.jpg",
+    "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
+  ];
+
   return (
-    <Container>
-      {/* TODO: se debe hacer el carrusel*/}
+    <>
+      <Carousel slides={slides}/>
+      <Container>
+        {/* TODO: se debe hacer el carrusel*/}
 
-      <div className="justify-between items-center flex my-16">
-        <Cards
-          imagen="/static/images/Logouoh.png"
-          titulo="Nosotros"
-          texto="El HPC de la UOH proporciona experiencia, capacidad de cómputo, servicios y capacitación para permitir la investigación intensiva en computación y ciencia de datos."
-        />
-        <Cards
-          imagen="/static/images/Logouoh.png"
-          titulo="Servicios"
-          texto="Nuestros servicios están diseñados para ofrecer soporte e innovación basada en supercómputo (HPC)."
-        />
-        <Cards
-          imagen="/static/images/Logouoh.png"
-          titulo="Infraestructura"
-          texto="El supercomputador más poderoso de Chile está disponible para los investigadores de todo el país."
-        />
-      </div>
+        <div className="justify-between items-center flex my-16">
+          <Cards
+            imagen="/static/images/Logouoh.png"
+            titulo="Nosotros"
+            texto="El HPC de la UOH proporciona experiencia, capacidad de cómputo, servicios y capacitación para permitir la investigación intensiva en computación y ciencia de datos."
+          />
+          <Cards
+            imagen="/static/images/Logouoh.png"
+            titulo="Servicios"
+            texto="Nuestros servicios están diseñados para ofrecer soporte e innovación basada en supercómputo (HPC)."
+          />
+          <Cards
+            imagen="/static/images/Logouoh.png"
+            titulo="Infraestructura"
+            texto="El supercomputador más poderoso de Chile está disponible para los investigadores de todo el país."
+          />
+        </div>
 
-      {/* TODO: se debe hacer lo de los eventos*/}
-      <div>
+        {/* TODO: se debe hacer lo de los eventos*/}
+        <div>
 
-      </div>
-    </Container>
+        </div>
+      </Container>
+    </>
   );
 }
